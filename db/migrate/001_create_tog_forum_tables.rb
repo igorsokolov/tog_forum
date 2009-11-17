f053401de60b8fc11b5fa40c6570b5df8c461a30
Alex Eiras <alexeiras@aspgems.com>
2009-11-17 17:55:41 +0100

    add sticky and locked attributes to topics

--- db/migrate/001_create_tog_forum_tables.rb
+++ db/migrate/001_create_tog_forum_tables.rb
@@ -5,17 +5,19 @@ class CreateTogForumTables < ActiveRecord::Migration
       t.integer  :user_id
       t.timestamps
     end
-    
+
     create_table :tog_forum_topics do |t|
       t.integer  :forum_id
       t.integer  :user_id
       t.string   :title
       t.text     :body
+      t.boolean  :sticky
+      t.boolean  :locked
       t.datetime :last_post_at
       t.integer  :last_post_by
       t.timestamps
     end
-    
+
     create_table :tog_forum_posts do |t|
       t.integer  :topic_id
       t.integer  :user_id
