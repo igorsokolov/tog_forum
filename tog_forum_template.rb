189be6e73309b129b437c4a2189090e9cd5d9e26
Igor Sokolov <igor.sokolov@gmail.com>
2009-12-24 11:15:24 +0200

    added acts_as_voteable to template

--- tog_forum_template.rb
+++ tog_forum_template.rb
@@ -1,5 +1,6 @@
 plugin 'white_list_model', :git => "git://github.com/imanel/white_list_model.git"
 plugin 'tog_forum', :git => "git://github.com/igorsokolov/tog_forum.git"
+plugin 'acts_as_voteable', :git => "git://github.com/inspiresynergy/acts_as_voteable.git", 
 route "map.routes_from_plugin 'tog_forum'"
 generate "update_tog_migration"
 rake "db:migrate"
