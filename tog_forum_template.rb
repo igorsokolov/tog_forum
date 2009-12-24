9a035c64460db86f59bb67d22256de9d89043ad0
Igor Sokolov <igor.sokolov@gmail.com>
2009-12-24 11:40:48 +0200

    changed template installation steps order

--- tog_forum_template.rb
+++ tog_forum_template.rb
@@ -1,7 +1,7 @@
 plugin 'white_list_model', :git => "git://github.com/imanel/white_list_model.git"
 plugin 'tog_forum', :git => "git://github.com/igorsokolov/tog_forum.git"
 route "map.routes_from_plugin 'tog_forum'"
-generate "update_tog_migration"
 plugin 'acts_as_voteable', :git => "git://github.com/igorsokolov/acts_as_voteable.git"
+generate "update_tog_migration"
 generate "acts_as_voteable_migration"
 rake "db:migrate"
